fb406ae7b01fd2d013e67fd28c3e6ea266630be7
Vladyslav Nechytailo <nechytailo.vladyslav@gmail.com>
2022-11-01 00:44:48 +0000

    Added bottom margin and increased font size in Navbar

--- src/Components/StatsMob.jsx
+++ src/Components/StatsMob.jsx
@@ -4,8 +4,8 @@ const StatsMob = (props) => {
   const temp = props.facts.temperature.match(/\d+/g).map(Number) + " °C";
 
   return (
-    <div className="cards md:hidden flex flex-col space-y-2 mt-7 text-white">
-      <div className="card_mob border-2 border-light_grey ">
+    <div className="cards md:hidden flex flex-col space-y-2 mt-7 mb-11 text-white">
+      <div className="card_mob border-2  border-light_grey ">
         <div className="justify-between mx-auto py-3.5 items-center flex  max-w-[279px]">
           <div className="card-title--mob opacity-50 font-sparta font-bold">
             ROTATION TIME
